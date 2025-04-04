84099ef791e57aacd4aedeec75fc7c1118112620
Nikita Mikhalevich <nik96mih@gmail.com>
2025-04-04 18:45:11 +0300

    doc fix

--- vitejs/pages.config.js
+++ vitejs/pages.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path'
 const pages = [
     {name: 'main', path: resolve(__dirname, '../index.html')},
     {name: 'offer_agreement', path: resolve(__dirname, '../doc/offer_agreement.html')},
-    {name: 'offer_agreement', path: resolve(__dirname, '../doc/privacy_policy.html')},
+    {name: 'privacy_policy', path: resolve(__dirname, '../doc/privacy_policy.html')},
 ];
 
 export default pages

f756d72fe278c623f6cd7ca1ef380dcdd8a1819b
Nikita Mikhalevich <nik96mih@gmail.com>
2025-04-04 18:38:35 +0300

    fix build

--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
 import '@styles/main.scss'
 
+export function setupCounter(element) {
+    let counter = 0
+    const setCounter = (count) => {
+      counter = count
+      element.innerHTML = `count is ${counter}`
+    }
+    element.addEventListener('click', () => setCounter(counter + 1))
+    setCounter(0)
+  }
+  
+  const expandableList = document.querySelector('.about__list-block');
+  const expandButton = document.querySelector('.expandButton');
+  
+  expandButton.addEventListener('click', () => {
+    expandableList.classList.toggle('expanded');
+    expandButton.textContent = expandableList.classList.contains('expanded') ? 'Скрыть мои услуги' : 'Показать остальное...';
+});
